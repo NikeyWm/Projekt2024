0712014f360a5a50aa2ca0e9c101009551fc01bf
lukimagpommes <wassermann.lukas@hakspittal.at>
2025-04-09 10:04:44 +0200

    Add database connection file

---
+++ dbconnection.php
@@ -0,0 +1,10 @@
+<?php
+try {
+    $dns = "mysql:host=localhost;dbname=checkmate;charset=utf8";
+
+    $pdo = new PDO($dns, 'root', '');
+} catch (PDOException $e) {
+    die("Fehler bei der Verbindung zur Datenbank: " . $e->getMessage());
+    
+}
+?>
